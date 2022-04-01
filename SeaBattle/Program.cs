66dcb76eae468708855c2947a53e0cb6fc6d13af
rozaogannesyan109@gmail.com <rozaogannesyan109@gmail.com>
2022-04-01 14:53:50 +0300

    Тест

--- SeaBattle/Program.cs
+++ SeaBattle/Program.cs
@@ -16,6 +16,9 @@ namespace SeaBattle
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+
+
+
         }
     }
 }
